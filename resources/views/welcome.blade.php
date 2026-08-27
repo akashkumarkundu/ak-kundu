74956165358dd083bebe79ea44e9aaf8402e9d26
akash kumar <akashkumarkundu2102121@gmail.com>
2026-08-27 18:59:42 +1000

    i just changed the header of view page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="Akash Kumar Kundu - Full Stack Laravel & Livewire Software Engineer Profile">
 
-        <title>Akash Kumar Kundu - Full Stack Developer Profile</title>
+        <title>Akash Kumar Kundu - Full Stack designer Profile</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
